d07fe4dcfcbb21ac633416976a26776661dd6804
Santiago Mendoza Ramirez <santiagomendozar@gmail.com>
2014-06-17 11:05:27 -0500

    Eliminar migraciones duplicadas.
    Eliminar migraciones duplicadas.

--- backend/app/models/Vehicle.php
+++ backend/app/models/Vehicle.php
@@ -3,6 +3,7 @@
 class Vehicle extends Eloquent {
 
     protected $table = 'vehicles';
+    protected $fillable = array('plate', 'color', 'model', 'capacity', 'type', 'users_id');
 
     public function users() {
         return $this->belongsTo('Vehicle');
