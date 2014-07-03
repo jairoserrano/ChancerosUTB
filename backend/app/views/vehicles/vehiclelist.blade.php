ff2462693d80d7bad203279e11450dfcbbe8ef55
Belkis Buelvas Castillo <belkis_buelvas06@hotmail.com>
2014-07-03 01:28:05 -0500

    Boton de añadir chance luego de añadir vehiculo
    Esta funcion permite al usuario tener una opcion mas rapida de crear un
    chance al momento de haber creado un vehiculo recientemente.

--- backend/app/views/vehicles/vehiclelist.blade.php
+++ backend/app/views/vehicles/vehiclelist.blade.php
@@ -6,7 +6,9 @@
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <!-- Bootstrap -->
         {{ HTML::style('recursos/css/bootstrap.min.css') }}
+        {{ HTML::style('recursos/css/stylevehiclelist.css') }}
         {{ HTML::style('recursos/css/style.css') }}
+
     </head>
     <body>
         @extends('layouts.master')
@@ -20,6 +22,20 @@
             @if($vehicle != null)
             @foreach($vehicle as $veh)
             @if($veh->status != 0)
+
+            <script type="text/javascript">
+                function items_click() {
+                    document.getElementById("hed").style.visibility = "hidden";
+                }
+            </script>
+            <div class="col-xs-9" id="hed">
+                <div class="panel panel-default" id="panelvehicle">
+                    <a class = "close" aria-hidden="true" href="#" onclick = "items_click()">&times</a>
+                    <h3>To create a new chance</h3>
+                    {{ HTML::link('/registerchance','Register Chance', array('class' => 'btn btn-success'), false) }}
+                </div> 
+            </div>
+
             <div class="col-md-6">   
                 <h4><strong>{{ $veh->brand }} {{ $veh->model }}</strong></h4>
                 <p><strong>Plate</strong>: {{ $veh->plate }} </p>
@@ -34,14 +50,15 @@
                     {{ Form::text('id', $veh->id, array('hidden' => 'true')) }}
                     {{ Form::button('Remove', array('type' => 'submit', 'class' => 'btn btn-danger')) }}
                     {{Form::close()}}
-                </div>
+                </div>    
             </div>
             @endif
             @endforeach
             @endif
-
             <div class="col-md-6">
+                <div class="col-md-5">
 
+                </div>
             </div>
         </div>
         <!--</div>-->
