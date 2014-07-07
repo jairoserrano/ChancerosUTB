658ea569dfca3254eeebd346eac6f2d9e1c821ac
Jorge Llamas Olivero <jotallamas@gmail.com>
2014-07-07 14:40:38 -0500

    Modificacion al update.sh
    Pequeña modificación al update:
    - No habrán mas modificaciones a la DB (A partir de aquí deben ser
    manuales)
    - Adición de Git Pull

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 cd /var/www/ChancerosUTB
+git pull
 git reset --hard
 cd /var/www/ChancerosUTB/backend
-php artisan migrate:reset
-php artisan migrate
-php artisan db:seed
 chown -R www-data:www-data *
