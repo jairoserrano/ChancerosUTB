34148a5649625dd69d227a64acaaced47686c07a
ingChristian27 <cristianoliveros_27@hotmail.com>
2014-07-07 15:57:07 -0500

    vista beta
    Prototipo vista perfil

--- backend/app/views/users/profile.blade.php
+++ backend/app/views/users/profile.blade.php
@@ -5,7 +5,7 @@
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <!-- Bootstrap -->
         {{ HTML::style('recursos/css/bootstrap.min.css') }}
-        {{ HTML::style('recursos/css/style.css') }}
+        
     </head>
     <body>        
         @extends('layouts.master')
@@ -13,18 +13,53 @@
         @parent
         @stop
         @section('content')
-        <div class="row" id="container_first">
-            <div class="col-md-12">
-                <h3>{{Auth::user()->name}} {{Auth::user()->lastname}}</h3>
-                <p>Email: {{Auth::user()->email}}</p>
-                <p>Username: {{Auth::user()->username}}</p>
-                <p>: {{Auth::user()->username}}</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="page-header">
+                        <h3>{{Auth::user()->name}} {{Auth::user()->lastname}}</h3>
+                        <h5> {{Auth::user()->email}}</h5>
+                        <button type="button"  class="label label-default">Edit</button>
+                    </div>
+
+                </div>
+                <div class="col-md-8">
+                
+
+                </div>
+              
+
             </div>
-        </div>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="recursos/js/jquery-1.9.1.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        {{ HTML::script('recursos/js/bootstrap.min.js') }}
-        @stop
+            <div class="row">
+                    <div class="col-md-4"></div>
+                        <div class="col-md-8">
+                            <div class="col-md-6">
+                                <div class="panel panel-success">
+                                    <div class="panel-heading">Panel heading without title</div>
+                                    <div class="panel-body">
+                                        Panel content
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-success">
+                                    <div class="panel-heading">Panel heading without title</div>
+                                    <div class="panel-body">
+                                        Panel content
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            <div class="row" id="container_first">
+
+
+
+            </div>
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="recursos/js/jquery-1.9.1.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            {{ HTML::script('recursos/js/bootstrap.min.js') }}
+            @stop
     </body>
 </html>
