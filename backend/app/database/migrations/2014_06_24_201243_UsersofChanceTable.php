f5e51adf4790a09406fb531b361e3518df6fac66
Jorge Llamas Olivero <jotallamas@gmail.com>
2014-07-07 15:50:13 -0500

    Revert "Commit Descartando campos: UsersOfChance"
    This reverts commit 592417d5c4f16615416e37e83fdf67fd304fb7f8.

--- backend/app/database/migrations/2014_06_24_201243_UsersofChanceTable.php
+++ backend/app/database/migrations/2014_06_24_201243_UsersofChanceTable.php
@@ -13,10 +13,10 @@ class UsersofChanceTable extends Migration {
     public function up() {
         Schema::create('usersofchance', function(Blueprint $table) {
             $table->increments('id');
-//            $table->integer('chances_id')->unsigned();
-//            $table->foreign('chances_id')->references('id')->on('chances')->onDelete('cascade');
-//            $table->integer('users_id')->unsigned();
-//            $table->foreign('users_id')->references('id')->on('users');
+            $table->integer('chances_id')->unsigned();
+            $table->foreign('chances_id')->references('id')->on('chances')->onDelete('cascade');
+            $table->integer('users_id')->unsigned();
+            $table->foreign('users_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
